78fbf46c3713a751f2f62a1a6cfcf58cfda96bbd
Tony Otim <tonyotim@Tonys-MacBook-Pro.local>
2024-11-25 22:18:53 -0600

    Added comments and changed data type to int

--- src/Assignment1/Prog1.java
+++ src/Assignment1/Prog1.java
@@ -1,18 +1,31 @@
 package Assignment1;
-
 import java.util.random.RandomGenerator;
-
 public class Prog1 {
     public static void main(String[] args) {
+
+        // RandomGenerator instance
         RandomGenerator rg  = RandomGenerator.getDefault();
-        double x = rg.nextDouble(1,10);
+
+        // Get a random integer x in the range 1..9
+        int x = rg.nextInt(1,10);
+
+        //Value of π
         double piValue = Math.PI;
-        double piToThePowerX = Math.pow(x, piValue);
-        System.out.println("(π raised to the power of x):" + piToThePowerX);
 
+        //Get value of π to the power x
+        double piToThePowerX = Math.pow(piValue, x);
 
-        double y = rg.nextDouble(3,14);
+        // Print the result of piToThePowerX
+        System.out.printf("π raised to the power of " + x + " : " + piToThePowerX + "\n");
+
+        // Get a random integer x in the range 1..9
+        int y = rg.nextInt(3,14);
+
+        //Get value of π to the power x
         double yToThePowerPi = Math.pow(y, piValue);
-        System.out.println("(y raised to the power of π): " + yToThePowerPi);
+
+        // Print the result of yToThePowerPi
+        System.out.printf(y + " raised to the power of π : " + yToThePowerPi);
+
     }
 }
