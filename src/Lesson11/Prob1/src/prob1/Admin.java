c8180471e20ea85b0d14e5e922adb4c9d2227ff2
Tony Otim <tonyotim@Tonys-MacBook-Pro.local>
2024-12-14 21:56:53 -0600

    Problem 1

--- src/Lesson11/Prob1/src/prob1/Admin.java
+++ src/Lesson11/Prob1/src/prob1/Admin.java
@@ -1,8 +1,15 @@
-package prob1;
+package Lesson11.Prob1.src.prob1;
 import java.util.*;
 public class Admin {
 	public static HashMap<Key, Student> processStudents(List<Student> students) {
-		//implement
-		return null;
+		HashMap<Key, Student> studentMap = new HashMap<>();
+
+		for (Student student: students){
+			Key key = new Key(student.getFirstName(), student.getLastName());
+			studentMap.put(key, student);
+		}
+
+		return studentMap;
+
 	}
 }
